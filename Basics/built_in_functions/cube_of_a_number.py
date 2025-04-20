a77fa283bd25a90f289f92e898c090784116688a
Sunidhi Nayak <86234399+sunidhi271@users.noreply.github.com>
2025-04-20 14:23:46 +0530

    Create cube_of_a_number.py
    Signed-off-by: Sunidhi Nayak <86234399+sunidhi271@users.noreply.github.com>

---
+++ Basics/built_in_functions/cube_of_a_number.py
@@ -0,0 +1,9 @@
+# Calculate the Cube of a Number
+# Here we are using "return" function to return the output of the funstion.
+
+def cube_of_number(n):
+    cube=n*n*n
+    return(cube)
+    
+print(cube_of_number(3))
+    

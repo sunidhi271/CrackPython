ebb457b12134d9ae1b49af80bf95730e375baf78
Sunidhi Nayak <86234399+sunidhi271@users.noreply.github.com>
2025-04-20 14:18:46 +0530

    Create nested_loop.py
    Signed-off-by: Sunidhi Nayak <86234399+sunidhi271@users.noreply.github.com>

---
+++ Basics/for_loop/nested_loop.py
@@ -0,0 +1,15 @@
+# Printing a Square Pattern with Nested For Loops
+
+n=5
+for i in range(n):
+    for j in range(n):
+        print("*", end="")
+    print()
+
+
+# OUTPUT: 
+# *****
+# *****
+# *****
+# *****
+# *****

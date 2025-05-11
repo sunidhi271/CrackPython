4b95c98db58cb871a4084a42e2a3a59a4be561cd
Sunidhi Nayak <86234399+sunidhi271@users.noreply.github.com>
2025-05-11 10:10:23 +0530

    Update string_in_list.py
    Signed-off-by: Sunidhi Nayak <86234399+sunidhi271@users.noreply.github.com>

--- Basics/data_structure/string_in_list.py
+++ Basics/data_structure/string_in_list.py
@@ -30,3 +30,23 @@ print(animals)                    #O/P: ['dog', 'cow', 'tiger', 'puppy', 'Lion',
 # If you want to print an item from the last, then you can use Negative Indexing
 print(animals[-1])              #O/P: Monkey
 print(animals[-4])              #O/P: tiger
+
+# The reverse() method directly modifies the original list. 
+# But, reversed() yields an iterator that facilitates looping through elements in reverse, but it doesn't alter the list.
+# sort() modifies the original list. 
+# On the other hand, sorted() returns a sorted version, preserving the original list unchanged.
+# You can conveniently use both sorted() and reversed() directly within loops for iterating over elements in a specific sequence.
+# By passing the key argument to sorted(), you can set custom sorting logic. 
+# The direction of sorting is determined by the reverse argument.
+
+numbers = ['Two', 'One', 'Three', 'Four', 'Zero']
+
+for number in sorted(numbers, key=len):
+    print(number, end=" ")         #O/P: Two One Four Zero Three
+    
+print(" ")
+print(numbers)                     #O/P: ['Two', 'One', 'Three', 'Four', 'Zero']
+print(" ")
+for num in sorted(numbers, key=len, reverse=True):
+    print(num, end=' ')            #O/P: Three Four Zero Two One
+

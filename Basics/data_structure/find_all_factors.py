5d21dc9711b64500e0b39c336231399aa13e7411
Sunidhi Nayak <86234399+sunidhi271@users.noreply.github.com>
2025-05-24 16:39:14 +0530

    Create find_all_factors.py
    Signed-off-by: Sunidhi Nayak <86234399+sunidhi271@users.noreply.github.com>

---
+++ Basics/data_structure/find_all_factors.py
@@ -0,0 +1,15 @@
+# Create a Python function named find_factors that determines all the factors of a given integer.
+# Output should be a list of integers which are factors of the given number.
+
+def find_factors(n):
+    list = []          #This is how we declare a list
+    i = 1
+    while i <= n:
+        if n % i == 0:
+            list += [i] 
+        i += 1 
+    return(list)
+    
+print(find_factors(0))   #Prints: []
+print(find_factors(12))  # Output: [1, 2, 3, 4, 6, 12]
+    
